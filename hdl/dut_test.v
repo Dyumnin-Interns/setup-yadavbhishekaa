c704e21f6f46f0d65c1ce7c62b96ba4dea263bfb
Abhishek Yadav <117608916+yadavbhishekaa@users.noreply.github.com>
2025-04-18 18:28:09 +0530

    Create dut_test.v

---
+++ hdl/dut_test.v
@@ -0,0 +1,16 @@
+module dut_test(
+    input wire a,
+    input wire b,
+    output wire y
+);
+    dut xor_gate_inst(  // Corrected module instantiation
+        .a(a),
+        .b(b),
+        .y(y)
+    );
+
+    initial begin  // Fixed 'initial' block syntax
+        $dumpfile("waves.vcd");  // Corrected quotes
+        $dumpvars(0, or_test);  // Fixed incorrect system task
+    end
+endmodule
